3254eb9324019e6a4572b15d83e45277c661a3cd
SeWoo-Kim <39786700+todnks@users.noreply.github.com>
2023-03-23 03:06:22 +0900

    feat: 브라우저가 처음열릴때 hash가 없을경우 기본 리스트 출력

--- src/App.ts
+++ src/App.ts
@@ -24,6 +24,7 @@ export default function App() {
 
   const hashName = location.hash.replace('#', '');
   const routes = {
+    '': storageData,
     '/': storageData,
     '/active': storageData.filter((data: todoListData) => data.completed === false),
     '/completed': storageData.filter((data: todoListData) => data.completed === true),
